cd011cf30fb71601141a4b044583423aa4e57858
tokuhirom <tokuhirom@gmail.com>
2012-07-05 11:18:42 +0900

    initial import

---
+++ lis.pl
@@ -0,0 +1,168 @@
+use strict;
+use warnings;
+use utf8;
+use 5.16.0;
+use autodie;
+
+package Lispl::Env {
+    sub new {
+        my ($class, $outer) = @_;
+        bless {
+            data => {
+            },
+            outer => $outer,
+        }, $class;
+    }
+    sub update {
+        my ($self, $vars, $args) = @_;
+        for my $i (0..0+@$vars-1) {
+            $self->data->{$vars->[$i]} = $args->[$i];
+        }
+    }
+    sub init_globals {
+        my $self = shift;
+        my %data = (
+            '+' => sub { $_[0] + $_[1] },
+            '*' => sub { $_[0] * $_[1] },
+            '-' => sub { $_[0] - $_[1] },
+            '<=' => sub { $_[0] <= $_[1] },
+            '>=' => sub { $_[0] >= $_[1] },
+            'say' => sub { say $_[0] },
+        );
+        for my $k (keys %data) {
+            $self->data->{$k} = $data{$k};
+        }
+    }
+    sub find {
+        my ($self, $key) = @_;
+        if (exists $self->data->{$key}) {
+            $self
+        } else {
+            if ($self->outer) {
+                $self->outer->find($key);
+            } else {
+                die "Name of $key not found";
+            }
+        }
+    }
+
+    sub outer { shift->{outer} }
+    sub data  { shift->{data} }
+}
+
+package Lispl {
+    sub new {
+        my $class = shift;
+        bless {
+            global_env => do {
+                my $env = Lispl::Env->new();
+                $env->init_globals;
+                $env;
+            }
+        }, $class;
+    }
+
+    sub evaluate {
+        my ($self, $x, $env) = @_;
+        $env ||= $self->{global_env};
+
+        if (!ref $x && $x !~ /^\d+$|^\d+\.\d+$/) { # it's variable
+            return $env->find($x)->data->{$x};
+        } elsif (!ref $x) {
+            return $x; # atom
+        } elsif ($x->[0] eq 'quote') {
+            shift @$x;
+            return $x;
+        } elsif ($x->[0] eq 'if') {
+            if ($self->evaluate($x->[1], $env)) {
+                return $self->evaluate($x->[2], $env);
+            } else {
+                return $self->evaluate($x->[3], $env);
+            }
+        } elsif ($x->[0] eq 'set!') {
+            my (undef, $var, $exp) = @$x;
+            $env->find($var)->data->{$var} = $self->evaluate($exp, $env);
+        } elsif ($x->[0] eq 'define') {
+            my (undef, $var, $exp) = @$x;
+            $env->data->{$var} = $self->evaluate($exp, $env);
+        } elsif ($x->[0] eq 'lambda') {
+            my (undef, $vars, $exp) = @$x;
+            return sub {
+                my $env = Lispl::Env->new($env);
+                $env->update($vars, \@_);
+                $self->evaluate($exp, $env);
+            };
+        } elsif ($x->[0] eq 'begin') { # (begin *exp)
+            my (undef, @exp) = @$x;
+            my $ret;
+            for (@exp) {
+                $ret = $self->evaluate($_, $env);
+            }
+            return $ret;
+        } else { # (proc exp*)
+            my @exps = map { $self->evaluate($_, $env) } @$x;
+            my $proc = shift @exps;
+            return $proc->(@exps);
+        }
+    }
+
+    sub parse {
+        my ($class, $src) = @_;
+        read_from($class->tokenize($src));
+    }
+
+    sub read_from {
+        my $tokens = shift;
+        if (0+@$tokens == 0) {
+            die "unexpected EOF while reading";
+        }
+        my $token = shift @$tokens;
+        given ($token) {
+            when ('(') {
+                my @L;
+                while ($tokens->[0] ne ')') {
+                    push @L, read_from($tokens);
+                }
+                shift @$tokens; # pop off ')'
+                return \@L;
+            }
+            when (')') {
+                die 'unexpected ")"';
+            }
+            default {
+                return $token;
+            }
+        }
+    }
+
+    sub tokenize($) {
+        my $class = shift;
+        local $_ = shift;
+        s/\(/ ( /g;
+        s/\)/ ) /g;
+        [grep { length $_ } split /\s+/, $_];
+    }
+}
+
+use File::Basename qw(dirname);
+use Getopt::Long;
+unless (caller) {
+    GetOptions(
+        'e=s' => \my $expression,
+    );
+    if ($expression) {
+        my $lispl = Lispl->new();
+        my $tree = $lispl->parse($expression);
+        my $ret = $lispl->evaluate($tree);
+        warn Dumper($ret);
+        exit;
+    }
+    while (1) {
+        print "lispl> ";
+        my $line = <> // last;
+        chomp $line;
+        my $tree = Lispl->parse($line);
+        use Data::Dumper;
+        warn Dumper($tree);
+    }
+}
---
+++ test.t
@@ -0,0 +1,69 @@
+use strict;
+use warnings;
+use utf8;
+use Test::More 0.96;
+use Data::Dumper;
+
+require 'lis.pl';
+
+subtest 'simple' => sub {
+    my $l = Lispl->new();
+    my $tree = $l->parse('(+ 2 3)');
+    is($l->evaluate($tree), 5);
+};
+
+subtest 'define' => sub {
+    my $l = Lispl->new();
+    my $tree = $l->parse('(begin (define x 4) x)');
+    is($l->evaluate($tree), 4);
+};
+
+subtest 'begin' => sub {
+    my $l = Lispl->new();
+    my $tree = $l->parse('(begin 1 2 3)');
+    is($l->evaluate($tree), 3);
+};
+
+subtest 'set!' => sub {
+    my $l = Lispl->new();
+    my $tree = $l->parse('(begin (define x 4) (set! x 5) x)');
+    is($l->evaluate($tree), 5);
+};
+die if $ENV{DEBUGGING}; 
+
+subtest 'if' => sub {
+    {
+        my $l = Lispl->new();
+        my $tree = $l->parse('(if 1 3 4)');
+        is($l->evaluate($tree), 3);
+    }
+    {
+        my $l = Lispl->new();
+        my $tree = $l->parse('(if 0 3 4)');
+        is($l->evaluate($tree), 4);
+    }
+};
+
+subtest 'area' => sub {
+    my $l = Lispl->new();
+    my $tree = $l->parse('(begin
+        (define area (lambda (r) (* 3.141592653 (* r r))))
+        (area 3)
+        )');
+    is($l->evaluate($tree), 28.274333877);
+};
+
+subtest 'fact' => sub {
+    my $l = Lispl->new();
+    my $tree = $l->parse('(begin
+         (define fact (lambda (n)
+            (if (<= n 1)
+                1
+                (* n (fact (- n 1))))))
+         (fact 10)
+        )');
+    is($l->evaluate($tree), 3628800);
+};
+
+done_testing;
+
